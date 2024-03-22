fe1cb482d0772781571e0eb77fbe391ddae2975c
Keenan Weise <kcweise@gmail.com>
2024-03-22 11:43:08 -0500

    Wrote Pull to pull in external API data and write to db.json

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Pull from './assets/Pull'
 
 function App() {
   const [count, setCount] = useState(0)
