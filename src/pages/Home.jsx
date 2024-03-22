7b7e0f6c35a66818680c06753fa57971107941f0
Christopher Thai <christopherthai.nyli@gmail.com>
2024-03-22 16:52:17 -0400

    Edit Home.jsx file to return the MainContent in the jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import MainContent from "../components/MainContent";
+
 function Home() {
   return (
-    <main>
-      <h1>Home!</h1>
-    </main>
-  )
+    <>
+      <main>
+        <h1>Home!</h1>
+      </main>
+      <MainContent />
+    </>
+  );
 }
 
 export default Home;
