fb13ff658230fc945efdb4f11c0a4911d2b380bc
Christopher Thai <christopherthai.nyli@gmail.com>
2024-03-23 17:18:51 -0400

    Fixed issue where the NavbBar was not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import MainContent from "./components/MainContent.jsx"
-import RecentBitesList from "./components/RecentBitesList.jsx"
-import 'bootstrap/dist/css/bootstrap.min.css'; //import bootstrap 
+import { Outlet } from "react-router-dom";
+import Header from './components/Header.jsx';
+import './index.css'
 
 function App() {
-  
-
   return (
-      <div>
-        <MainContent/>
-        <RecentBitesList/>
-      </div>
-  )
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
