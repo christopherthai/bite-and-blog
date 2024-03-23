98a142461d224640e9d6b76a103490d7c5c73a99
Christopher Thai <christopherthai.nyli@gmail.com>
2024-03-23 16:12:49 -0400

    Fix bug where pages were not display

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import MainContent from "./components/MainContent.jsx"
 import RecentBitesList from "./components/RecentBitesList.jsx"
@@ -13,20 +12,6 @@ function App() {
         <RecentBitesList/>
       </div>
   )
-=======
-import NavBar from "./components/NavBar";
-import { Outlet } from "react-router-dom";
-import Header from './components/Header.jsx';
-import './index.css'
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
-  );
->>>>>>> 18d00b62bbed2ed96564230adb5db66c8f17c716
 }
 
 export default App;
