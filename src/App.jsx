547118ff4f1afddc2b425decc9281f0fccbc5379
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-03-23 16:02:39 -0400

    1st pull-error came up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 import MainContent from "./components/MainContent.jsx"
 import RecentBitesList from "./components/RecentBitesList.jsx"
-import 'bootstrap/dist/css/bootstrap.min.css'; //import bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom";
+import Header from './components/Header.jsx';
+import './index.css'
 
 function App() {
   
@@ -11,22 +14,12 @@ function App() {
       <div>
         <MainContent/>
         <RecentBitesList/>
+        <Header />
+        <Outlet />
+        <NavBar />
       </div>
   )
-=======
-import NavBar from "./components/NavBar";
-import { Outlet } from "react-router-dom";
-import Header from './components/Header.jsx';
-import './index.css'
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
-  );
->>>>>>> 18d00b62bbed2ed96564230adb5db66c8f17c716
 }
+
 
 export default App;
--- src/components/RecentBitesList.jsx
+++ src/components/RecentBitesList.jsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-
 function RecentBitesList() {
 
     const [hoveredCard, setHoveredCard] = useState(null);
@@ -26,7 +24,7 @@ function RecentBitesList() {
             position: 'absolute', 
             top : '20%', 
             right: '50px',
-            backgroundImage: 'url("src/components/hotdog.jpeg")', 
+            backgroundImage: 'url("src/images/hotdog.jpeg")', 
             backgroundSize: 'cover',
             filter: hoveredCard === 1 ? 'blur(30px)' : 'none',
           transition: 'filter 0.5s ease'
@@ -60,7 +58,7 @@ function RecentBitesList() {
             position: 'absolute', 
             top: '40%', 
             right: '50px', 
-            backgroundImage: 'url("src/components/fac.webp")', 
+            backgroundImage: 'url("src/images/fac.webp")', 
             backgroundSize: 'cover',
             filter: hoveredCard === 2 ? 'blur(30px)' : 'none',
           transition: 'filter 0.5s ease'
@@ -87,7 +85,7 @@ function RecentBitesList() {
             height: '15rem', 
             position: 'absolute', 
             top: '60%', right: '50px', 
-            backgroundImage: 'url("src/components/pizza.jpg")', 
+            backgroundImage: 'url("src/images/pizza.jpg")', 
             backgroundSize: 'cover',
             filter: hoveredCard === 3 ? 'blur(30px)' : 'none',
           transition: 'filter 0.5s ease'
