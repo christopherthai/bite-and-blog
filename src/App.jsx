90a19ad2cc449c051facfa18df058f587d33b8c8
Keenan Weise <kcweise@gmail.com>
2024-03-23 08:31:38 -0500

    Display dynamic pictures and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import NavBar from "./components/NavBar";
 import { Outlet } from "react-router-dom";
 import Header from './components/Header.jsx';
+import AddBite from "./pages/AddBite.jsx";
 
 function App() {
   return (
     <div>
-      <Header />
-      <NavBar />
-      <Outlet />
+      {/*<Header />*/}
+      {/*<NavBar />*/}
+     {/* <Outlet />*/}
+     <AddBite/>
     </div>
   );
 }
--- src/pages/AddBite.jsx
+++ src/pages/AddBite.jsx
@@ -1,8 +1,65 @@
 
-function AddBite() {
-  return (
-    <div>AddBite</div>
-  )
-}
+import React, { useState, useEffect } from 'react';
 
+function AddBite() {
+
+  
+    const [meals, setMeals] = useState([]);
+    const [displayImages, setDisplayImages] = useState([]);
+
+    useEffect(() =>{
+
+      fetch('http://localhost:4000/meals/')
+      .then((response) => response.json())
+      .then((data) => {
+        setMeals(data);
+        console.log(data);
+        //Upon initial render interate thru and append 5 random meal to set displayImages state
+        const initialImages = [];
+        for (let i = 0; i<5; i++) {
+          initialImages.push(data[Math.floor(Math.random() * data.length)]);
+        }
+        setDisplayImages(initialImages)
+      });
+    }, []);
+
+
+    useEffect(() => {
+      // Set an interval to cycle through meal images
+      const intervalId = setInterval(() => {
+        if(meals.length > 0) {
+        
+          const randomIndexToUpdate = Math.floor(Math.random() * displayImages.length);
+
+          const randomMealImage = meals[Math.floor(Math.random() * meals.length)];
+
+          const updatedImages = [...displayImages];
+          updatedImages[randomIndexToUpdate] = randomMealImage;
+          setDisplayImages(updatedImages);
+        }
+
+      }, 2000); // Change image every 3 seconds
+      
+          return () => clearInterval(intervalId); // Cleanup interval on component unmount
+        }, [meals, displayImages]);
+  
+        if (meals.length === 0) return <div>Loading images...</div>;
+      
+
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Featured Meals</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+        {displayImages.map((meal, index) => (
+          <img
+            key={index}
+            src={meal.strMealThumb}
+            alt={meal.strMeal}
+            style={{ width: '18%', objectFit: 'cover', maxHeight: '300px' }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 export default AddBite;
