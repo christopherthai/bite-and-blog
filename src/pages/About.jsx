831efc3de1937f923c2a9c229bec192969cbdf77
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-03-26 12:13:57 -0500

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "../index.css";
 import sharedmeal from "../images/sharedmeal.jpeg";
 import cookingathome from "../images/cookingathome.jpeg";
+import cooking from "../images/cooking.jpg.webp";
+import dinnerwithfriends from "../images/dinnerwithfriends.webp";
 
 function About() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const images = [sharedmeal, cookingathome, dinnerwithfriends, cooking];
+
   return (
     <>
       <main className="wrapper">
@@ -16,9 +33,15 @@ function About() {
           dishes from your favorite restaurant, recipes from your kitchen at
           home and beyond. Life is cheddar when shared with you!
         </p>
-        <div>
-          <img src={sharedmeal} alt="Shared Meal" />
-          <img src={cookingathome} alt="cooking at home" />
+        <div className="image-container-about">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Image ${index}`}
+              className={index === currentImageIndex ? "visible" : "hidden"}
+            />
+          ))}
         </div>
       </main>
     </>
